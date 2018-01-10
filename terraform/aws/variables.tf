7d60f7cd2e40c8bc3d49284aa1ef65f5a5800987
Yamikuronue <yamikuronue@gmail.com>
2018-01-10 15:23:44 +0000

    add burner key

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -30,12 +30,12 @@ variable "key_name" {
 
 # You can update this to match your personal public SSH key half.
 variable "public_key" {
-  default="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDABuFmeNzJtKfq/BhI5okytxj9a7J+eDlUCPozZdasQyXq3DSy4pOUO3brYMSiTCUX0eQseUVXjzUO4C7OojZKZ8dONaiUrBvj8icKB137mDzq5iSAZrbdnIQdjiNZC3/DQ6W0PMHk4SZez0Z5oKikbP/l/E8zRxnfWJHXiogYogOdtqxZdx4SshDy2iPsReINFJNIwJJB0cbZB35fhxRAghSmf3MWjkPeHzLE+6fZD3HszzZ7g7ngDN1E5MRqH06tqeRDboTypOrH5CcUjergq2xwJ9/eSdbtrKY4uGG5U/6RTo0hA/kcLW9AmBsHlCvpn5YnLKhswETCkEOCsqHZ thedevilsvoice@grimoire"
+  default="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDLNIBUZ1VmJmM8gGDYXQn+WG9A0kURajhLMsCpqEgUiv5x9+ESD+VoUVRyz+zxxazLQ+5IakjfgxrcFn9COZrYl3zEvyTqd+jOB/09easzln+4n+o1J3kNDkvz+RFDSHXlMqR5DOb4w0n+E4IrXFiQFlHDksBrw/mQHsAmq0HgfanMQ24IktUmdk7E2jLJW02nbS//BYR2B5N+SU6XN0Ylc4DeRanbUajAWi/+W2gGbOlCiTgt5tZdQdPypnKsbHAmk+aCSeyUcK0K19yISx+LXivLKe6WtTAl6SlNKgLeUZ3woOfr6RQQFgoAd+3erIFvO4xnljZqPSleIs3LCjGsi2rSxH/EzMBawYd0CNhVi3CncNsT1pKDaZ6yIQXvkV/OT53IYbHE6LgsJXrxDd91AabQq2hlDUsju8m6Hhus+sV/cPlGUZIHycIHgMRheSUQCpZvo0E8ShshhhM6dIxIdfnuxgU3pLy0tti1LESDOoUl5AFPH4tSNlc4IAfwVsmWsl6FTwRtN2ZP4bt3ZormxZGJf6Tkf2i28m3MVQg4DExiaSPPi7tmuIN6GGco2yznvnPWvd1XFqV6K3sbHwOsr+QEUukYNrN1syRjQOUj2/Ivli17SUVRjip4xpgljYq6km7E6T1US4ZyjWJwFbdQOYgQSUAoVvQvZTkUZQ+LXw== yamikuronue@gmail.com"
   description = "SSH Public key half to use for centos on the new host"
 }
 
 variable "key_path" {
-  default = "/home/thedevilsvoice/.ssh/do_terra_rsa"
+  default = "/home/yamikuronue/.ssh/id_codemash"
   description = "Path to your SSH private key half on local dev machine"
 }
 
